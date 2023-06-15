cb72881cfa648c06918569e1bbcf0ce5f482f602
Galforno <ciccolellagiuseppe01@gmail.com>
2023-06-15 12:40:34 +0200

    Initial Bootstrap commit
    -Import bootstrap su index
    -Aggiunto lo stile alla pagina di login

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react"
 import axios from 'axios'
 import { useNavigate } from "react-router-dom"
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
 
 export default function LoginForm(){
     const [username, setUsername]=useState("")
@@ -24,11 +31,32 @@ export default function LoginForm(){
     }
 
     return (
-        <form
-            onSubmit={handleSubmit}>
-            <input value={username} onChange={e=>setUsername(e.target.value)}/>
-            <input value={password} onChange={e=>setPassword(e.target.value)} type="password"/>
-            <button type="submit">Log In</button>
-        </form>
+        <>
+            <Container className="position-absolute top-50 start-50 translate-middle">
+                <Form onSubmit={handleSubmit}>
+                    <Row>
+                        <Col md={3}></Col>
+                        <Col md={6}>
+                            <FloatingLabel label="Username" className="mb-3">
+                                <Form.Control value={username} placeholder="Username" onChange={e=>setUsername(e.target.value)}/>
+                            </FloatingLabel>
+                        </Col>
+                        <Col md={3}></Col>
+                    </Row>
+                    <Row>
+                        <Col md={3}></Col>
+                        <Col md={6}>
+                            <FloatingLabel label="Password" className="mb-3">                
+                                <Form.Control value={password} placeholder="Password" onChange={e=>setPassword(e.target.value)} type="password"/>
+                            </FloatingLabel>
+                        </Col>
+                        <Col md={3}></Col>
+                    </Row>
+                    <Row xs={4} md={6} className="justify-content-center">
+                        <Button variant="outline-primary" type="submit">Log In</Button>
+                    </Row>
+                </Form>
+            </Container>
+        </>
     )
 }
